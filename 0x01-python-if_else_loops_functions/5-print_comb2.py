68c0886bf4717999a181d3b58c2710f6d91dad06
sophiaDawodu <sophiadawodu@gmail.com>
2022-06-29 17:14:27 -0700

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(100):
-    if n < 99:
-        print("{:0>2d},".format(n), end="")
+for i in range(100):
+    if i < 99:
+        print("{:0>2d}, ".format(i), end="")
     else:
-        print("{:0>2d}".format(n))
+        print("{:0>2d}".format(i))
